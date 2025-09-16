bfc240d715c49135a767e691d7467ee75841ea23
tforest <thomas@frst.tf>
2025-09-16 12:12:15 -0700

    fix config line parsing

--- parsing.py
+++ parsing.py
@@ -113,7 +113,8 @@ def parse_config(config_file, args=None):
     param = {}
     with open(config_file, "rt") as config:
         line=config.readline()
-        while line != "":
+        # if the line can be stripped, it's not empty
+        while line.strip():
             if line[0] != "#":
                     param[line[:-1].split(": ")[0]] = line[:-1].split(": ")[1].strip()
             line = config.readline()
